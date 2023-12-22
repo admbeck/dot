e1096066620a55e83f3f1ca6d345146629902d4a
AdmBeck <admbeck@outlook.com>
2023-12-23 02:32:51 +0500

    fix: more hotkeys for linting

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -54,6 +54,21 @@ return {
         end,
         desc = "Try linting current file",
       },
+      {
+        "<leader>ck",
+        function()
+          vim.diagnostic.reset(nil, 0)
+        end,
+        desc = "Reset linting for current file"
+      },
+      {
+        "<leader>cx",
+        function()
+          local ft = vim.filetype.match({buf = 0})
+          require("lint").linters_by_ft[ft] = {}
+        end,
+        desc = "Disable linting for current filetype"
+      }
     },
     opts = {
       linters_by_ft = {
