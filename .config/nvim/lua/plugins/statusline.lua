575dd14e2955f0d89a5876e3081acd1ad7e828fe
AdmBeck <admbeck@outlook.com>
2024-01-10 16:50:22 +0500

    fix: made tabline full width

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -145,22 +145,14 @@ return {
           lualine_z = {},
         },
         tabline = {
-          lualine_a = {
+          lualine_a = {},
+          lualine_b = {
             {
               "tabs",
               mode = 1,
-              -- fmt = function(name, context)
-              --   -- Show + if buffer is modified in tab
-              --   local buflist = vim.fn.tabpagebuflist(context.tabnr)
-              --   local winnr = vim.fn.tabpagewinnr(context.tabnr)
-              --   local bufnr = buflist[winnr]
-              --   local mod = vim.fn.getbufvar(bufnr, '&mod')
-              --
-              --   return name .. (mod == 1 and ' +' or '')
-              -- end
+              max_length = vim.o.columns,
             },
           },
-          lualine_b = {},
           lualine_c = {},
           lualine_x = {},
           lualine_y = {},
