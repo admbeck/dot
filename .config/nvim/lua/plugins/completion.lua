230516244913152d2e898eb774c2283005f5afa7
AdmBeck <admbeck@outlook.com>
2024-02-22 16:28:47 +0500

    feat: added treesitter extension plugins

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -126,4 +126,28 @@ return {
       })
     end,
   },
+  { -- nvim-ts-autotag: Automatically add closing tags for HTML and JSX
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    opts = {},
+  },
+  { -- nvim-treesitter-context: Show context of the current function
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    enabled = true,
+    opts = {
+      enable = false,
+      mode = "cursor",
+      max_lines = 3
+    },
+    keys = {
+      {
+        "<leader>zt",
+        function()
+          require("treesitter-context").toggle()
+        end,
+        desc = "Toggle Treesitter Context",
+      },
+    },
+  },
 }
