5710445a60fe664be23121325027239daceb56aa
AdmBeck <admbeck@outlook.com>
2024-02-23 09:53:15 +0500

    feat: added indent line plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -115,4 +115,44 @@ return {
       top_down = false,
     },
   },
+  { -- indent-blankline.nvim: indent guides
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>zi",
+        function()
+          require("ibl").setup_buffer(0, {
+            enabled = not require("ibl.config").get_config(0).enabled,
+          })
+        end,
+        desc = "Toggle indent lines",
+      }
+    },
+    opts = {
+      -- enabled = false,
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+          "markdown"
+        },
+      },
+    },
+  },
 }
