8fa404120c4e406d650c605ecdfd984229e37f03
admbeck <admbeck@outlook.com>
2024-06-24 16:03:02 +0500

    fix: disabled neorg

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,34 +1,43 @@
-return { -- neorg: org mode for neovim
-  "nvim-neorg/neorg",
-  -- enabled = false,
-  requires = {
-    "nvim-treesitter/nvim-treesitter",
-    "hrsh7th/nvim-cmp",
-  },
-  cmd = "Neorg",
-  build = ":Neorg sync-parsers",
-  config = function()
-    local WIKIPATH = os.getenv("WIKIPATH")
-    require("neorg").setup({
-      load = {
-        ["core.defaults"] = {},
-        ["core.completion"] = {
-          config = { engine = "nvim-cmp" },
-        },
-        ["core.integrations.nvim-cmp"] = {},
-        ["core.concealer"] = {},
-        ["core.dirman"] = {
-          config = {
-            workspaces = {
-              notes = WIKIPATH .. "/neorg",
-            },
-            default_workspace = "notes",
-            autochdir = true,
+return {
+  { -- neorg: org mode for neovim
+    "nvim-neorg/neorg",
+    enabled = false,
+    requires = {
+      "nvim-treesitter/nvim-treesitter",
+      "hrsh7th/nvim-cmp",
+    },
+    dependencies = { "luarocks.nvim" },
+    cmd = "Neorg",
+    -- build = ":Neorg sync-parsers",
+    config = function()
+      local WIKIPATH = os.getenv("WIKIPATH")
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.completion"] = {
+            config = { engine = "nvim-cmp" },
           },
+          ["core.integrations.nvim-cmp"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = WIKIPATH .. "/neorg",
+              },
+              default_workspace = "notes",
+              autochdir = true,
+            },
+          },
+          ["core.export.markdown"] = {},
+          ["core.integrations.treesitter"] = {},
         },
-        ["core.export.markdown"] = {},
-        ["core.integrations.treesitter"] = {},
-      },
-    })
-  end,
+      })
+    end,
+  },
+  {
+    "vhyrro/luarocks.nvim",
+    enabled = false,
+    priority = 1000,
+    config = true,
+  }
 }
