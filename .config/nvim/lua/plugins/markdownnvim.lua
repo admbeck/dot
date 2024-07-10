5d8025c9860e7682200036b6819b45ad1a237cf3
admbeck <admbeck@outlook.com>
2024-07-10 13:33:49 +0500

    feat: updated config to newest version

--- .config/nvim/lua/plugins/markdownnvim.lua
+++ .config/nvim/lua/plugins/markdownnvim.lua
@@ -3,14 +3,50 @@ return { -- markdown.nvim: concealement and pretty notes
   requires = { "nvim-treesitter/nvim-treesitter" },
   ft = "markdown",
   opts = {
-    headings = { "● ", "○ ", "▶ ", "◆ ", "◇ ", "⤷ " },
-    bullets = { "● ", "○ ", "▶ ", "◆ " },
+    -- https://neovim.io/doc/user/treesitter.html - reference for highlights
+    link = {
+      hyperlink = "",
+    },
+    checkbox = {
+      unchecked = {
+        icon = "󰄰 ",
+      },
+      checked = {
+        icon = "󰄴 ",
+      },
+      custom = {
+        todo = { -- pending
+          rendered = "󰍷 ",
+          highlight = "@markup.link.label"
+        },
+        canceled = {
+          raw = "[_]",
+          rendered = "󱃓 ",
+          highlight = "@comment",
+        },
+        paused = { -- on hold
+          raw = "[=]",
+          rendered = "󰏦 ",
+          highlight = "@markup.link.label",
+        },
+        more_input = { -- needs further input
+          raw = "[?]",
+          rendered = " ",
+          highlight = "@comment.warning",
+        },
+        urgent = {
+          raw = "[!]",
+          rendered = "󰗖 ",
+          highlight = "@comment.error",
+        },
+      },
+    },
   },
   keys = {
     {
       "<leader>zm",
       "<cmd>lua require('render-markdown').toggle()<CR>",
-      desc = "toggle markdown concealement",
+      desc = "Toggle markdown concealement",
     },
   },
 }
